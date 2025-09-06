e32326e831856a38eb39cbeaa421ad7632f2ae19
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-06 16:50:56 +0000

    Implemented restaurant entity

--- src/main/java/com/example/dinemaster/model/Restaurant.java
+++ src/main/java/com/example/dinemaster/model/Restaurant.java
@@ -1,9 +1,64 @@
-/*
- *
- * You can use the following import statements
- * 
- * import javax.persistence.*;
- * 
- */
+package com.example.dinemaster.model;
 
-// Write your code here
+import javax.persistence.*;
+
+@Entity
+@Table(name = "restaurant")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String address;
+    private String cuisineType;
+    private int rating;
+
+    // Constructors
+    public Restaurant() {}
+
+    public Restaurant(int id, String name, String address, String cuisineType, int rating) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.cuisineType = cuisineType;
+        this.rating = rating;
+    }
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCuisineType() {
+        return cuisineType;
+    }
+    public void setCuisineType(String cuisineType) {
+        this.cuisineType = cuisineType;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
