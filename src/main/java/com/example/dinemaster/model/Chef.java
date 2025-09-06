839f7f83e626ffd916294b72f70c79f533c0faf4
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-06 16:52:09 +0000

    Implemented chef entity model

--- src/main/java/com/example/dinemaster/model/Chef.java
+++ src/main/java/com/example/dinemaster/model/Chef.java
@@ -1,9 +1,76 @@
-/*
- *
- * You can use the following import statements
- * 
- * import javax.persistence.*;
- * 
- */
+package com.example.dinemaster.model;
 
-// Write your code here
+import javax.persistence.*;
+
+@Entity
+@Table(name = "chef")
+public class Chef {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private String expertise;
+    private int experienceYears;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurantId", referencedColumnName = "id")
+    private Restaurant restaurant;
+
+    // Constructors
+    public Chef() {}
+
+    public Chef(int id, String firstName, String lastName, String expertise, int experienceYears, Restaurant restaurant) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.expertise = expertise;
+        this.experienceYears = experienceYears;
+        this.restaurant = restaurant;
+    }
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+}
